899cf45bed42188eb76b32edde51992343e26a36
gseba <gseba7@gmail.com>
2019-09-01 18:21:22 -0700

    Updated API File

--- README.md
+++
@@ -1,2 +0,0 @@
-# Python-API-Challenge
-Unit 6 API Homework Assignment - What's the Weather Like?
--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,2 @@
 # OpenWeatherMap API Key
-api_key = "YOUR KEY HERE!"
+api_key = "f88a57488b8ca8a85134b9ea51b6f8ba"
